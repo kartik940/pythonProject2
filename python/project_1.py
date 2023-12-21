2650f2e40434a6b83e5a374431d7b9b4844cb008
kartik <kartikeytiwari620@gmsil.com>
2023-12-21 23:57:59 +0530

    push

--- python/project_1.py
+++ python/project_1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import csv
-df = pd.read_csv("python/sample.csv", sep = ',', header= 'infer', encoding = 'latin1')
+df = pd.read_csv("sample.csv", sep = ',', header= 'infer', encoding = 'latin1')
 print(df)
 
 
