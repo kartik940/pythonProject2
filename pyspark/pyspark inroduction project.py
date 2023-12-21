854af5b848a6ba1784395fe502622defd81a0155
kartik <kartikeytiwari620@gmsil.com>
2023-12-22 02:08:06 +0530

    push

--- pyspark/pyspark inroduction project.py
+++ pyspark/pyspark inroduction project.py
@@ -1,6 +1,6 @@
 # read file in pyspark and then create data frame and create another dataframe and join them
+
 from pyspark.sql import SparkSession
-# creating a variable
-spark = SparkSession.builder.getOrCreate()
-df_spark = spark.read.csv("IPL Matches 2008-2020.csv", inferSchema = True, header = True)
+spark = SparkSession.builder.appName("ipl_data").getOrCreate()
+df_spark = spark.read.csv("pyspark/ipl_data.csv", header=True, inferSchema=True)
 df_spark.show()
