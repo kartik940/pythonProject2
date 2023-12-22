f3a209553a2ccc80ee8b8b2e93bff2c71a377a67
kartik <kartikeytiwari620@gmsil.com>
2023-12-23 00:48:14 +0530

    push

--- pyspark/pyspark_inroduction_project.py
+++ pyspark/pyspark_inroduction_project.py
@@ -1,16 +1,10 @@
-# read file in pyspark and then create data frame and create another dataframe and join them
-# create a function which will read any(csv) file and will return dataframe
-
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("ipl_data").getOrCreate()
-# df_spark = spark.read.csv(r"pyspark\ipl_data.csv", header=True, inferSchema=True)
-# df_spark.show()
-
-# defined a function
 def file_to_data_frame(path):
-    # read file
-    df_spark = spark.read.csv(path,header=True, inferSchema=True)
-    # return function
-    return df_spark
-data = file_to_data_frame(r"pyspark\ipl_data.csv")
-data.show()
+    return spark.read.csv(path, header=True, inferSchema=True)
+df_1 = file_to_data_frame(r"pyspark/Book1.csv")
+df_2 = file_to_data_frame(r"pyspark/sample.csv")
+df_3 = file_to_data_frame(r"pyspark/sample.csv")
+join_df = df_1.join(df_2, df_1.number == df_2.number, 'left')
+join_df.show()
+
